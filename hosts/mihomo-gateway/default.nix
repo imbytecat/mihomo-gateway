28ba0aa9640f1e2f3ee6a08884cb9ecf1de34548
imbytecat <imbytecat@gmail.com>
2026-02-05 05:50:51 +0800

    fix(gateway): 修复 networkd/dhcpcd 冲突和 root 密码警告

--- hosts/mihomo-gateway/default.nix
+++ hosts/mihomo-gateway/default.nix
@@ -11,13 +11,20 @@
   # System
   system.stateVersion = "24.11";
 
-  # Networking
+  # Networking (use systemd-networkd, required by lxc-container.nix)
   networking = {
     hostName = "mihomo-gateway";
-    useDHCP = true;
+    useNetworkd = true;
+    useDHCP = false;  # Managed by networkd below
     firewall.enable = false;  # We use nftables directly
     # Use traditional resolv.conf (LXC uses host's DNS)
     useHostResolvConf = lib.mkForce true;
+  };
+
+  # DHCP via networkd
+  systemd.network.networks."50-eth" = {
+    matchConfig.Name = "eth*";
+    networkConfig.DHCP = "yes";
   };
 
   # Disable systemd-resolved (conflicts with useHostResolvConf)
@@ -39,6 +46,9 @@
     settings.PermitRootLogin = "yes";  # TODO: Adjust for production
   };
 
-  # Root user (for initial access)
-  users.users.root.initialPassword = "nixos";
+  # Root user (for initial access, override lxc-container.nix empty password)
+  users.users.root = {
+    initialPassword = "nixos";
+    initialHashedPassword = lib.mkForce null;
+  };
 }
