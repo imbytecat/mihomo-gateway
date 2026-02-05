764f4981782d74b76726e2d806dd8b08547c2425
imbytecat <imbytecat@gmail.com>
2026-02-05 20:47:55 +0800

    fix(lxc): 使用 proxmox-lxc.nix 模块修复 PVE 导入失败

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,7 @@
   imports = [
     ./modules/tproxy.nix
     ./modules/mihomo.nix
-    "${modulesPath}/virtualisation/lxc-container.nix"
+    "${modulesPath}/virtualisation/proxmox-lxc.nix"
     "${modulesPath}/profiles/minimal.nix"
     "${modulesPath}/profiles/perlless.nix"
     "${modulesPath}/profiles/headless.nix"
@@ -21,14 +21,16 @@
   system.stateVersion = "25.11";
   i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" ];
 
+  # 使用 NixOS 管理 hostname，不使用 PVE 的
+  proxmoxLXC.manageHostName = true;
   networking = {
     hostName = "mihomo-gateway";
-    useNetworkd = true;
-    useDHCP = false;
     firewall.enable = false;
-    useHostResolvConf = lib.mkForce true;
+    useHostResolvConf = lib.mkForce true;  # 使用宿主机 DNS
   };
 
+  # 网络由 proxmox-lxc.nix 自动配置 (systemd-networkd + DHCP)
+  # 添加 eth* 匹配规则
   systemd.network.networks."50-eth" = {
     matchConfig.Name = "eth*";
     networkConfig.DHCP = "yes";
@@ -38,12 +40,10 @@
   time.timeZone = "Asia/Shanghai";
   environment.systemPackages = with pkgs; [ micro ];
 
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "prohibit-password";
-      PasswordAuthentication = false;
-    };
+  # openssh 由 proxmox-lxc.nix 默认启用，只需配置认证
+  services.openssh.settings = {
+    PermitRootLogin = "prohibit-password";
+    PasswordAuthentication = false;
   };
 
   users.users.root = {
