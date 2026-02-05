f2fabab2faeb26f697c486a8ab32a768cac4ee1c
imbytecat <imbytecat@gmail.com>
2026-02-05 20:50:19 +0800

    style: 移除冗余注释

--- configuration.nix
+++ configuration.nix
@@ -21,16 +21,13 @@
   system.stateVersion = "25.11";
   i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" ];
 
-  # 使用 NixOS 管理 hostname，不使用 PVE 的
   proxmoxLXC.manageHostName = true;
   networking = {
     hostName = "mihomo-gateway";
     firewall.enable = false;
-    useHostResolvConf = lib.mkForce true;  # 使用宿主机 DNS
+    useHostResolvConf = lib.mkForce true;
   };
 
-  # 网络由 proxmox-lxc.nix 自动配置 (systemd-networkd + DHCP)
-  # 添加 eth* 匹配规则
   systemd.network.networks."50-eth" = {
     matchConfig.Name = "eth*";
     networkConfig.DHCP = "yes";
@@ -40,7 +37,6 @@
   time.timeZone = "Asia/Shanghai";
   environment.systemPackages = with pkgs; [ micro ];
 
-  # openssh 由 proxmox-lxc.nix 默认启用，只需配置认证
   services.openssh.settings = {
     PermitRootLogin = "prohibit-password";
     PasswordAuthentication = false;
