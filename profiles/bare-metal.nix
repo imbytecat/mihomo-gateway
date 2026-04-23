2fda4cc4706aa1f3a9ecd339d5cc48355a5ac74a
imbytecat <imbytecat@gmail.com>
2026-04-23 16:59:27 +0800

    fix(bare-metal): initrd 加 virtio 驱动，修复 KVM/QEMU VM 装完进 emergency
    默认 initrd 只有 ahci/nvme/sata 系列，KVM/QEMU 虚拟磁盘（virtio-blk/virtio-scsi）在真 reboot 后看不到，initrd 等 90s 超时然后进 emergency mode，又因 root 锁死而彻底卡死。kexec 安装阶段没遇到是因为 kexec 环境用的是 nixos-images 的完整 kernel。

--- profiles/bare-metal.nix
+++ profiles/bare-metal.nix
@@ -9,4 +9,12 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # KVM/QEMU 目标需要；stock initrd 默认只有 ahci/nvme/sata，虚拟磁盘会看不到导致 boot 后进 emergency
+  boot.initrd.availableKernelModules = [
+    "virtio_pci"
+    "virtio_blk"
+    "virtio_scsi"
+    "virtio_net"
+  ];
 }
